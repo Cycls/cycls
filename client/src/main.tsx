5387aef38b167770e359ba9015e120ce352c9e97
Mohammed Alrujayi <58256600+aburjg@users.noreply.github.com>
2026-03-14 00:20:37 +0000

    Default dark mode and language from system preferences

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Default to dark mode
-document.body.classList.add("dark");
+// Default to system preferences
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.body.classList.add("dark");
+}
+if (navigator.language.startsWith("ar")) {
+  document.documentElement.lang = "ar";
+  document.documentElement.dir = "rtl";
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
